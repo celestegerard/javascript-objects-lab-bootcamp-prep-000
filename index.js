a30429b6fc70e6e34f63d0dbe7392ca565862c58
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-25 18:02:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key] = value;
+  return recipes;
+} 
+
+function deleteFromObjectByKey(object, key) {
+  var newRecipe = Object.assign({}, obj)
+  delete newObj.foo
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  return delete object.key;
+}
+
